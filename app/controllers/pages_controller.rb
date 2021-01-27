2c3505c99238e2f8e0cd137326ab52b16b062e0f
Lionel <>
2021-01-27 17:28:48 +1100

    fix 8

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,14 @@
 class PagesController < ApplicationController
   def home
-    # @current_bag = @current_user.bags.first
+    return redirect_to login_path unless @current_user.present?
+
+    @current_bag = @current_user.bags.first
     @api_list = PokeApi.get(pokemon: {limit: 1200, offset: 0})
     @rand_pokemon = @api_list.results.sample
-    # @pokemon1 = @current_bag.pokemons.first
-    # @pokemon2 = @current_bag.pokemons.second
-    # @pokemon3 = @current_bag.pokemons.third
-    # @attack1 = @pokemon1.attacks.first
+    @pokemon1 = @current_bag.pokemons.first
+    @pokemon2 = @current_bag.pokemons.second
+    @pokemon3 = @current_bag.pokemons.third
+    @attack1 = @pokemon1.attacks.first
   end
 
 end
