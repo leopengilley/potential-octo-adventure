47e2f0699fe3e9648fb0e63db2505c1c90e3363e
Lionel <>
2021-01-27 16:38:56 +1100

    fix attempt 1

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -25,8 +25,23 @@ class PokemonsController < ApplicationController
     end
   end
 
+  def edit
+    @pokemon = Pokemon.find(params[:id])
+  end
+
+  def update
+    pokemon = Pokemon.new(pokemon_params)
+    if params[:front, :back].present?
+      req = Cloudinary::Uploader.upload(params[:front, :back])
+      pokemon.image = req["public_id"]
+    end
+    pokemon.update_attributes(pokemon_params)
+    pokemon.save
+    redirect_to(pokemons_index_path)
+  end
+
   private
   def pokemon_params
-    params.require(:pokemon).permit(:name)
+    params.require(:pokemon).permit(:name, :attack, :front, :back)
   end
 end
