6717fc44c84dbf862bf84a1de840d2360a81bb03
Lionel <>
2021-01-27 17:04:40 +1100

    fix 4

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -12,36 +12,35 @@ class PokemonsController < ApplicationController
     @pokemon = Pokemon.new
   end
 
-  def create
-    pokemon = Pokemon.new(pokemon_params)
-    if params[:front, :back].present?
-      req = Cloudinary::Uploader.upload(params[:front, :back])
-      pokemon.image = req["public_id"]
-      pokemon.save
-    end
-    redirect_to pokemons_index_path
-    else
-      render :new
-    end
-  end
+  # def create
+  #   pokemon = Pokemon.new(pokemon_params)
+  #   if params[:front, :back].present?
+  #     req = Cloudinary::Uploader.upload(params[:front, :back])
+  #     pokemon.image = req["public_id"]
+  #     pokemon.save
+  #   end
+  #   redirect_to pokemons_index_path
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def edit
-    @pokemon = Pokemon.find(params[:id])
-  end
+  # def edit
+  #   @pokemon = Pokemon.find(params[:id])
+  # end
 
-  def update
-    pokemon = Pokemon.new(pokemon_params)
-    if params[:front, :back].present?
-      req = Cloudinary::Uploader.upload(params[:front, :back])
-      pokemon.image = req["public_id"]
-    end
-    pokemon.update_attributes(pokemon_params)
-    pokemon.save
-    redirect_to(pokemons_index_path)
-  end
+  # def update
+  #   pokemon = Pokemon.new(pokemon_params)
+  #   if params[:front, :back].present?
+  #     req = Cloudinary::Uploader.upload(params[:front, :back])
+  #     pokemon.image = req["public_id"]
+  #   end
+  #   pokemon.update_attributes(pokemon_params)
+  #   pokemon.save
+  #   redirect_to(pokemons_index_path)
+  # end
 
   private
   def pokemon_params
     params.require(:pokemon).permit(:name, :attack, :front, :back)
   end
-end
